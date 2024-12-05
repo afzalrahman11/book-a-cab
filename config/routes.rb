376e7ded9596ffc3769f9d3b92c44203497b2446
Afzal Rahman <afzalrahman123@gmail.com>
2024-12-05 08:40:53 +0530

    Add devise gems, separate customer-owner home pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+        sessions: "users/sessions",
+        registrations: "users/registrations"
+      }
+
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Render dynamic PWA files from app/views/pwa/*
@@ -6,4 +11,5 @@ Rails.application.routes.draw do
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
   root "home#index"
+  resources :users, only: [ :index, :show ]
 end
